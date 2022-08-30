d440438499f24d1bfab631ac92e50f2e61523ec7
Arrow technologies <Arrow technologies@DESKTOP-P254STU>
2022-08-30 21:19:47 +0530

    commit

---
+++ JavaDemo/src/Interface/Animal.java
@@ -0,0 +1,11 @@
+package Interface;
+
+/**
+ * @author Gayatri Vastrad
+ *
+ */
+public interface Animal {
+	public void eat();
+	public void makesSound();
+
+}
---
+++ JavaDemo/src/Interface1/I1.java
@@ -0,0 +1,6 @@
+package Interface1;
+
+public interface I1 {
+	void display();
+
+}
