a447ccc2c4d1cd0de05eb836765a1459f3d2d7a5
Arrow technologies <Arrow technologies@DESKTOP-P254STU>
2022-09-13 18:57:12 +0530

    commit

---
+++ JavaDemo/src/Generics/GenericClassTest.java
@@ -0,0 +1,24 @@
+package Generics;
+class GenericTest<T>
+{
+	T obj;
+	void add(T obj)
+	{
+		this.obj=obj;
+	}
+	
+	T get()
+	{
+		return obj;
+	}
+}
+
+public class GenericClassTest {
+
+	public static void main(String[] args) {
+		GenericTest<String> obj=new GenericTest<String>();
+		obj.add("Hello");
+		System.out.println(obj.get());
+	}
+
+}
